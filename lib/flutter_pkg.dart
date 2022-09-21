27ae66c22cd65a57c600877f4fec99f55e259635
toum <nanhtavadh@gmail.com>
2022-09-22 02:24:23 +0700

    Made some changes

--- lib/flutter_pkg.dart
+++ lib/flutter_pkg.dart
@@ -1,7 +1,27 @@
 library flutter_pkg;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final Widget child;
+  const CustomButton({Key? key, @required this.onPressed, required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(16.0),
+          backgroundColor: Colors.blue,
+          elevation: 9.0,
+          textStyle: const TextStyle(
+            fontSize: 20,
+            color: Colors.white
+          ),
+          foregroundColor: Colors.white,
+        ),
+        child: child);
+  }
 }
--- test/flutter_pkg_test.dart
+++ test/flutter_pkg_test.dart
@@ -4,9 +4,8 @@ import 'package:flutter_pkg/flutter_pkg.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
   });
 }
