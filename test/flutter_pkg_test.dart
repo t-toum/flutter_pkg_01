8391428309761c4a9579ae52a40dc1372a6543ca
toum <nanhtavadh@gmail.com>
2022-09-22 02:34:15 +0700

    Made some changes

--- test/flutter_pkg_test.dart
+++ test/flutter_pkg_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_pkg/flutter_pkg.dart';
+// import 'package:flutter_pkg/flutter_pkg.dart';
 
 void main() {
   test('adds one to input values', () {
